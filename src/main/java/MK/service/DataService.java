eb6f75dbffc852db1df1d264ffb28bbacd9742da
Magdalena Karolczuk <284987@pw.edu.pl>
2019-03-16 19:50:07 +0100

    creating dto and service

--- src/main/java/MK/service/DataService.java
+++ src/main/java/MK/service/DataService.java
@@ -15,7 +15,7 @@ public class DataService {
     private CustomerRepositoryImpl customerRepository = new CustomerRepositoryImpl();
     private ProductRepositoryImpl productRepository = new ProductRepositoryImpl();
 
-    public void showTheMostExpensiveProduct() {
+    public void showwTheMostExpensiveProduct() {
         Map<Category, Product> mapCategoryProduct = productRepository.findAll()
                 .stream()
                 .collect(Collectors.toMap(p->p.getCategory(), c->c));
